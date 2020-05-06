1232b115abed00309bc9e8fd2c838a692ee9237d
Ben <limhanyangb@gmail.com>
2020-05-06 20:24:26 +0800

    Updates

---
+++ precache-manifest.620d36528ecde258722ccd572827eba9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468b38e6adb9c2b4b791edad9585875d",
+    "url": "/react-teave/index.html"
+  },
+  {
+    "revision": "2ff84e8183ff72378c8d",
+    "url": "/react-teave/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "83ecaaa138252439b93f",
+    "url": "/react-teave/static/css/main.4346d8ca.chunk.css"
+  },
+  {
+    "revision": "2ff84e8183ff72378c8d",
+    "url": "/react-teave/static/js/2.12449639.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-teave/static/js/2.12449639.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83ecaaa138252439b93f",
+    "url": "/react-teave/static/js/main.f9961f47.chunk.js"
+  },
+  {
+    "revision": "ff2a4e02b5076038d958",
+    "url": "/react-teave/static/js/runtime-main.0867e89b.js"
+  }
+]);
